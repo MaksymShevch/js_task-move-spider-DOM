d493fa827d9efb48a3fee97217970a882a098623
Max <gcrazy007@gmail.com>
2024-10-11 21:58:56 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  if (e.target.className === 'wall') {
+  if (e.target.classList.contains('wall')) {
     const x = e.clientX;
     const y = e.clientY;
     const field = e.target.getBoundingClientRect();
@@ -10,19 +10,20 @@ document.addEventListener('click', (e) => {
     const borderX = (e.target.offsetWidth - e.target.clientWidth) / 2;
     const borderY = (e.target.offsetHeight - e.target.clientHeight) / 2;
 
-    const maxLeft = field.width - spider.offsetWidth - borderX * 2;
-    const maxTop = field.height - spider.offsetHeight - borderY * 2;
+    const maxLeft = Math.max(0, field.width - spider.offsetWidth - borderX * 2);
+
+    const maxTop = Math.max(
+      0,
+      field.height - spider.offsetHeight - borderY * 2,
+    );
 
     let spiderLeft = x - field.left - spider.offsetWidth / 2 - borderX;
     let spiderTop = y - field.top - spider.offsetHeight / 2 - borderY;
 
-    spiderLeft = spiderLeft < 0 ? 0 : spiderLeft;
-    spiderLeft = spiderLeft > maxLeft ? maxLeft : spiderLeft;
+    spiderLeft = Math.min(maxLeft, spiderLeft);
+    spiderTop = Math.min(maxTop, spiderTop);
 
-    spiderTop = spiderTop < 0 ? 0 : spiderTop;
-    spiderTop = spiderTop > maxTop ? maxTop : spiderTop;
-
-    spider.style.left = `${spiderLeft}px`;
-    spider.style.top = `${spiderTop}px`;
+    spider.style.left = spiderLeft + 'px';
+    spider.style.top = spiderTop + 'px';
   }
 });
